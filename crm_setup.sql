cf3afdba0d8134be4a0a4f4e1d0e23b79ab99c86
verneh <verneh@gmail.com>
2023-06-30 20:34:53 +0200

    Add files via upload

---
+++ crm_setup.sql
@@ -0,0 +1,49 @@
+
+-- dashboard data setup. loads data when new ones arrive. does not include setup through IAM or event notification.
+
+-- access warehouse
+USE WAREHOUSE DS_DEV_QQ_WH;
+
+-- create schema
+CREATE SCHEMA CRM_DASHBOARD;
+
+-- create table
+CREATE OR REPLACE TABLE CRM_DASHBOARD (
+    SNAPSHOT_DATE DATE,
+    COUNTRY_CODE_UPPER VARCHAR(15),
+    CUSTOMER NUMBER,
+    CCM NUMBER(12,6),
+    NEW_CUSTOMER VARCHAR(15),
+    SALES NUMBER(12,6),
+    LOYALTY VARCHAR(15),
+    EMAIL_OPTIN VARCHAR(15),
+    ACCOUNT VARCHAR(15),
+    OCM NUMBER(12,6),
+    OCM_SALES NUMBER(12,6),
+    ORDERS NUMBER
+);
+
+-- create stage with S3 bucket name with file format to be loaded.
+CREATE OR REPLACE STAGE ext_crm_dashboard
+ URL = 's3://dpe_bucket/crm_dashboard/'
+ CREDENTIALS=(AWS_KEY_ID='xxxxxxxx' AWS_SECRET_KEY='xxxxxxxx')
+ FILE_FORMAT = (TYPE = 'CSV');
+
+-- create our pipe using the stage layer.
+ CREATE OR REPLACE PIPE crm_dashboard_pipe AUTO_INGEST=TRUE AS
+ COPY INTO CRM_DASHBOARD
+ FROM ext_crm_dashboard
+ FILE_FORMAT = (TYPE = 'CSV')
+ ON_ERROR = 'CONTINUE';
+
+ -- create view to extract.
+CREATE OR REPLACE VIEW crm_dashboard_view as
+  SELECT "customer",
+    "country_code_upper",
+    "orders"
+  FROM CRM_DASHBOARD;
+
+ -- create a stream to check for updates.
+ CREATE OR REPLACE STREAM crm_dashboard_stream ON VIEW crm_dashboard_view;
+
+ SELECT * FROM crm_dashboard_view;
